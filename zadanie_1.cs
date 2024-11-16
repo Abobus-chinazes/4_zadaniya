d4f4bc59f2000d0c6fe4262162f559d133fc23e6
Abobus-chinazes <mr.berek420@gmail.com>
2024-11-16 14:07:00 +0300

    Add files via upload

---
+++ zadanie_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rkis_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+            else
+            {
+                Console.WriteLine("Оба числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
